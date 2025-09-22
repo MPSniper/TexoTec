6996b35e8cff7262dc18ead01d833a6fe846a40a
Mohammad Pourbarfeh <mpsnipersb@gmail.com>
2025-09-22 20:58:10 +0330

    updated dashboard page

--- frontend/src/pages/DashboardPage.jsx
+++ frontend/src/pages/DashboardPage.jsx
@@ -1,11 +1,16 @@
 import React from "react";
+import "./dashboardPage.scss";
 import Sidebar from "../components/SidebarComponent/sidebar.jsx";
+import DashnoardProjectSection from "../components/dashboardProjectSection/dashnoardProjectSection.jsx";
 
 const DashboardPage = () => {
   return (
-    <Sidebar>
-      <div>DashboardPage</div>
-    </Sidebar>
+    <div className="dashboardPage">
+      <Sidebar />
+      <div className="dashboardPage__content">
+        <DashnoardProjectSection />
+      </div>
+    </div>
   );
 };
 
